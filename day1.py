1727a81f448b887e727d86787640537f81b72ffa
tomer <101682824+tomernahum@users.noreply.github.com>
2025-12-01 06:22:33 -0500

    :

--- day1.py
+++ day1.py
@@ -3,23 +3,49 @@ from reader import read_file_to_list
 
 dial = 0
 
-def turnDial(input, dir, amount):
+def turnDialO(input, dir, amount):
     out = 0 
     if dir == "L":
         out = (input - amount)
     elif dir == 'R':
         out = (input + amount)
     
-    
+
     clicks = 0
+
+
+    if out == 0:
+        clicks += 1
+    
     while out < 0:
         out += 100
         clicks += 1
     while out > 99:
         out -= 100
         clicks += 1
+    
     return out, clicks
 
+
+def turnDial(input, dir, amount):
+    dial = input
+    zero_passes = 0
+
+    
+    for i in range(amount):
+        if dir == 'L':
+            dial -= 1
+            if dial == -1:
+                dial = 99
+                zero_passes += 1
+        elif dir == 'R':
+            dial += 1
+            if dial == 100:
+                dial = 0
+                zero_passes += 1
+    # if dial == 0:
+    #     zero_passes -= 1
+    return dial, zero_passes
 
 print((99 + 10) % 100)
 print((5 - 10) % 100)
@@ -29,7 +55,8 @@ print(turnDial(50, 'R', 1000))
 print(turnDial(50, 'L', 68))
 
 
-input = read_file_to_list("./day1-input.txt")
+input = read_file_to_list("./day1-ex.txt")
+# input = read_file_to_list("./day1-input.txt")
 
 
 dial = 50
@@ -37,12 +64,16 @@ zero_count = 0
 for line in input:
     dir = line[0]
     amt = int(line[1:])
-    
-    dial, clicks = turnDial(dial, dir, amt)
-    zero_count += clicks
 
+
+    print(f'dial start: {dial}, doing: {line}', end="")
+    
+    dial, zero_passes = turnDial(dial, dir, amt)
+    zero_count += zero_passes
     if dial == 0:
         zero_count += 1
+
+    print(f"  |  dial end: {dial},  new zero count: {zero_count}")
 
 print(zero_count)
 
