960d6102b912a8dfd2f9a43c40b8579d033bb0f7
tomer <101682824+tomernahum@users.noreply.github.com>
2025-12-04 00:19:46 -0500

    attempt 1

---
+++ 4/day4.py
@@ -0,0 +1,81 @@
+ex_input = """
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+""".strip()
+
+def inputToList(input):
+    rows = []
+    for line in input.split("\n"):
+        row = []
+        for char in line:
+            row.append(char)
+        rows.append(row)
+    return rows
+
+def main(input):
+    rows = inputToList(input)
+    
+    # determine adjacency
+    # brute force cause why not and im undereducated on competitive programming
+
+    valid_count = 0
+    for row_i, row in enumerate(rows):
+        for col_i, item in enumerate(row):
+            if item == ".":
+                continue
+
+            # 3-8 adjacent coords
+            adjacent_blocks = getAdjacentBlocks(row_i, col_i, rows)
+            num_filled_adj = 0
+            for pos_x, pos_y in adjacent_blocks:
+                adj_block_value = rows[pos_x][pos_y]
+                if adj_block_value == "@" or adj_block_value == "x":
+                    num_filled_adj += 1
+            
+            if num_filled_adj < 4:
+                rows[row_i][col_i] = "x"
+                valid_count += 1
+
+    # print(rows)
+    for row in rows:
+        print("".join(row))
+
+    print(valid_count)
+    
+
+def getAdjacentBlocks(row_i, col_i, rows):
+    max_col = len(rows) - 1
+    max_row = len(rows[0]) - 1
+    min_col = 0
+    min_row = 0
+
+    pos_1 = (row_i - 1, col_i - 1)
+    pos_2 = (row_i - 1, col_i)
+    pos_3 = (row_i - 1, col_i + 1)
+    pos_4 = (row_i, col_i - 1)
+    pos_5 = (row_i, col_i + 1)
+    pos_6 = (row_i + 1, col_i - 1)
+    pos_7 = (row_i + 1, col_i)
+    pos_8 = (row_i + 1, col_i + 1)
+
+
+    for pos in [pos_1, pos_2, pos_3, pos_4, pos_5, pos_6, pos_7, pos_8]:
+        if pos[0] < min_row or pos[0] > max_row or pos[1] < min_col or pos[1] > max_col:
+            continue
+        yield pos
+    
+
+
+# print(list(getAdjacentBlocks(0, 0, inputToList(ex_input))))
+# print(list(getAdjacentBlocks(3, 3, inputToList(ex_input))))
+
+
+main(ex_input)
