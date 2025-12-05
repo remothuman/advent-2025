ad66ae71ea6b104ef1ea7fb92f476374636d3afb
tomer <101682824+tomernahum@users.noreply.github.com>
2025-12-05 05:06:35 -0500

    ,

--- 5/day5.py
+++ 5/day5.py
@@ -14,7 +14,7 @@ ex_input = """
 with open("./vibe/day5_input.txt", "r") as f:
     real_input = f.read()
 
-def main(input):
+def main_1(input):
     ranges, available_ids = input.split("\n\n")
     ranges = ranges.split("\n")
     available_ids = available_ids.split("\n")
@@ -32,9 +32,31 @@ def main(input):
             if i in available_set:
                 fresh_set.add(i)
                 num_fresh += 1
-        print(f"checked range {range_str}, found {num_fresh} fresh")
+        print(f"checked range {range_str}, total is {num_fresh} fresh")
     
     print(num_fresh)
+    # ok this version not fast enough
+        
+    
+def main_2(input):
+    ranges, available_ids = input.split("\n\n")
+    ranges = ranges.split("\n")
+    available_ids = available_ids.split("\n")
+    
+    def get_range(range_index):
+        start, end = ranges[range_index].split("-")
+        return range(int(start), int(end) + 1)
+    
+    current_range_index = 0
+    current_range = get_range(current_range_index)
+    current_available_index = 0
+    while True:
+        available_item = available_ids[current_available_index]
+        current_fresh_item = ranges[current_range_index]
+        next_fresh_item = current_range.next()
+        
+    
+    
 
-main(real_input)
-# ok this version not fast enough
+main_2(ex_input)
+
