1a1b42dee9fabaed4dabbd90d7b3bfd90444cdcd
tomer <101682824+tomernahum@users.noreply.github.com>
2025-12-05 05:14:43 -0500

    attempt

--- 5/day5.py
+++ 5/day5.py
@@ -43,20 +43,37 @@ def main_2(input):
     ranges = ranges.split("\n")
     available_ids = available_ids.split("\n")
     
-    def get_range(range_index):
-        start, end = ranges[range_index].split("-")
-        return range(int(start), int(end) + 1)
+    def get_fresh_items_iter():
+        for range_str in ranges:
+            start, end = range_str.split("-")
+            start, end = int(start), int(end)
+            for i in range(start, end + 1):
+                yield i
+    def get_available_ids_iter():
+        for available_id in available_ids:
+            yield int(available_id)
     
-    current_range_index = 0
-    current_range = get_range(current_range_index)
-    current_available_index = 0
-    while True:
-        available_item = available_ids[current_available_index]
-        current_fresh_item = ranges[current_range_index]
-        next_fresh_item = current_range.next()
-        
+    fresh_items = []
+    fresh_items_iter = get_fresh_items_iter()
+    available_ids_iter = get_available_ids_iter()
     
+    current_fresh_item = next(fresh_items_iter)
+    current_available_item = next(available_ids_iter)
+    try: 
+        while True:
+            if current_fresh_item == current_available_item:
+                fresh_items.append(current_fresh_item)
+                print(f"found fresh item {current_available_item}")
+                current_fresh_item = next(fresh_items_iter)
+                # current_available_item = next(iter(available_ids))
+            elif current_fresh_item < current_available_item:
+                current_fresh_item = next(fresh_items_iter)
+            else: # current_fresh_item > current_available_item
+                current_available_item = next(available_ids_iter)
+    except StopIteration:
+        pass
+    print(len(fresh_items))
     
 
-main_2(ex_input)
+main_2(real_input)
 
