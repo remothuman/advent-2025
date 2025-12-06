fd73eb8fe635b99091fc41fce9b3449155d89ba4
tomer <101682824+tomernahum@users.noreply.github.com>
2025-12-06 04:27:19 -0500

    attempt. still not quite right but probably soon

--- 6/day6.py
+++ 6/day6.py
@@ -56,7 +56,7 @@ def parse_input_part1(input:str, intify=True):
     return out2
 
 
-def parse_input_part2(input:str):
+def parse_input_part2_bad(input:str):
     # convert into 2D array
     rows = input.split("\n")
     print(input)
@@ -95,8 +95,51 @@ def parse_input_part2(input:str):
     for problem_i in range(num_problems):
         for row in out_1_rows[:-1]: # exclude op;erator row
             pass
-            
+
+# def parse_input_part1112(input:str):
+#     """
+#     Plan:
+#     1. break into each problem
+#     2. continue on
+#     """
+#     problems_unprocessed = parse_input_part2(input)
     
+def parse_input_part2(input:str):
+    rows = input.split("\n")
+    num_chars_per_row = len(rows[0])
+    
+    num_rows = len(rows)
+    
+    problems = []
+    current_problem = []
+    current_problem_operator = None
+    for char_x in range(num_chars_per_row):
+        number = ""
+        for row in rows[:-1]:
+            number += row[char_x]
+        if rows[-1][char_x] != " ":
+            current_problem_operator = rows[-1][char_x]
+            
+        if number == " "*(num_rows-1):
+            # this is start of new problem
+            current_problem.append(current_problem_operator)
+            problems.append(current_problem)
+            current_problem = []
+            current_problem_operator = None
+        else:
+            # continue on to next char_x aka number
+            current_problem.append(number)
+    
+    # strip out whitespace and intify
+    out = []
+    for problem in problems:
+        out_problem = []
+        for number in problem[:-1]:
+            out_problem.append(int(number.strip()))
+        out_problem.append(problem[-1])
+        out.append(out_problem)
+    
+    return out
     
             
 def convert_problem_to_correct_format(problem:list[str]):
@@ -125,6 +168,11 @@ with open("./input_trunc.txt", "r") as f:
 # print(main(ex_input, part2=True))
 # print(main(real_input))
 
-print(parse_input_part2(ex_input))
+# print(parse_input_part2(ex_input))
+# for r in parse_input_part2(trunc_input):
+#     print(r)
+    
+print(main(ex_input, part2=True))
+print(main(real_input, part2=True))
 
 
